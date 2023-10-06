7181fe8f28bc3777f8a8cbdfc34be3fc6315cece
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-07 02:40:49 +0500

    sidebar added

--- js.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".menu-button");
-const sidebarContent = document.querySelector(".sidebar-content");
-
-menuButton.addEventListener("click", () => {
-  sidebarContent.classList.toggle("sidebar-content-open");
-});
---
+++ script.js
@@ -0,0 +1,6 @@
+var menuicon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector("side-bar")
+
+menuicon.onclick = function(){
+  sidebar.classList/.toggle("small-side-bar")
+}
