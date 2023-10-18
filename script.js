98ba60b9ab30ca212b529e761ef090bfdbbec313
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-19 00:48:36 +0500

    some

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var menuicon = document.querySelector("menu-icon")
-var sidebar = document.querySelector("side-bar")
+var menuicon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector(".side-bar")
 
 menuicon.onclick = function(){
-  sidebar.classList/.toggle("small-side-bar")
+  sidebar.classList.toggle(".small-side-bar")
+
 }
+
+// function toggleSidebar() {
+//   const sidebar = document.querySelector(".side-bar");
+//   sidebar.classList.toggle("open");
+
+// }
+
+// const menuIcon = document.querySelector("#menu");
+// menuIcon.addEventListener('click', toggleSidebar);
